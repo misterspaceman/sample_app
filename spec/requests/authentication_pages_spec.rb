28007919d8e8b5b1e5411e84ea65f858e56930f3
Mike Devlin <mpdevlin@gmail.com>
2014-05-01 22:20:02 -0500

    Work on sign-in module

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,12 +11,18 @@ describe "Authentication" do
 		it { should have_title('Sign in') }
 	end
 
-	describe "sign in" do
+	describe "signin" do
 		before { visit signin_path }
 
 		describe "with invalid information" do
+			before { click_button "Sign in" }
 			it { should have_selector('div.alert.alert-error')}
 			it { should have_title('Sign in') }
+
+			describe "after visiting another page" do
+				before { click_link "Home" }
+				it { should_not have_selector('div.alert.alert-error') }
+			end
 		end
 
 		describe "with valid information" do
